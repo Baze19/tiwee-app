2f387787ad904e69d9c68ea8babb9226881052e9
baze19 <bazemontana@gmail.com>
2023-01-22 05:25:00 +0100

    newest commit

---
+++ build/precache-manifest.319884dd20d07bda0c89e0298b8b1afb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cead8a75fcd07201a172992bf15e95c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2350fbbbeaff32807116",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "669d1e86c471173977ba",
+    "url": "/static/js/2.ef63ed04.chunk.js"
+  },
+  {
+    "revision": "5a4b68170e3d24a2b07f2666e20e831e",
+    "url": "/static/js/2.ef63ed04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2350fbbbeaff32807116",
+    "url": "/static/js/main.fbff1cdc.chunk.js"
+  },
+  {
+    "revision": "51fcd79bb8592a6dc931",
+    "url": "/static/js/runtime-main.7bd8fb2a.js"
+  }
+]);
